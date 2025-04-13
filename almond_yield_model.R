214c07baf552bbe8efa43d071c7440314300a098
Aoverbye <amandaoverbye@gmail.com>
2025-04-13 18:17:45 -0500

    First draft function.

--- almond_yield_model.R
+++ almond_yield_model.R
@@ -5,15 +5,21 @@
 
 
 
-#So we would need to filter the month to equal february
+#So we would need to filter the month to equal February
 
 almond_yield_test <- function(clim){
-    group_by(year, month) %>%
+    group_by(year) %>%
         summarise(
-            avg_feb_temp = mean(tmin_c[month == 2]),
-            total_jan_precip = sum(precip[month == 1])
+            avg_feb_temp = mean(tmin_c[month == 2],na.rm = TRUE),
+            total_jan_precip = sum(precip[month == 1],na.rm = TRUE)
         ) %>% 
-        mutate(anomaly = -0.015 * avg_feb_temp - 0.0046 * avg_feb_temp^2 - .07 * total_jan_percep + .0034 * total_jan_percep1^2 +.28)
-    return(anomoly)
+        mutate(anomaly = -0.015 * avg_feb_temp - 0.0046 * avg_feb_temp^2 -
+                   0.07 * total_jan_precip + 0.0043 * total_jan_precip^2 + 0.28)
+    
+    anomaly_min <- min(function_test2$anomaly, na.rm = TRUE)
+    anomaly_max <- max(function_test2$anomaly, na.rm = TRUE)
+    anomaly_mean <- mean(function_test2$anomaly, na.rm = TRUE)
+    
+    return(c(anomaly_min, anomaly_max, anomaly_mean))
 }
 
