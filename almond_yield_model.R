ea27c892aa2b0697f17d5ab7dfefaa9ab6a48b41
Aoverbye <amandaoverbye@gmail.com>
2025-04-13 14:31:52 -0500

    Made the month defining part of the function.

--- almond_yield_model.R
+++ almond_yield_model.R
@@ -1,15 +1,19 @@
 # A function for almond yield anomaly as a response to climate 
 
-almond_yield_model <- function(data){
-    min = min_daily_temp_feb
-    min = min_daily_temp_feb
-}
-
 #Y = "0.015Tn,2 " 0.0046T2
 #n;2
 
 
-almond_yield_test <- function(feb_temp1, percep1){
+data <- filter(month = 2)
+
+#So we would need to filter the month to equal february
+
+almond_yield_test <- function(clim){
+    group_by(year, month) %>%
+        summarise(
+            feb_temp = mean(tmin_c[month == 2]),
+            jan_precip = sum(precip[month == 1])
+        )
     anomoly = -0.015 * feb_temp1 - 0.0046 * feb_temp1^2 - .07 * percep1 + .0034 * percep1^2 +.28
     return(anomoly)
 }
