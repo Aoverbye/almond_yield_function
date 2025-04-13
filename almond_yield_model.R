21526b57d4f62ce1db97f7990e7fb85d969f08cf
Aoverbye <amandaoverbye@gmail.com>
2025-04-13 18:34:00 -0500

    Editted almond function, added important notes.

--- almond_yield_model.R
+++ almond_yield_model.R
@@ -1,14 +1,19 @@
-# A function for almond yield anomaly as a response to climate 
 
-#Y = "0.015Tn,2 " 0.0046T2
-#n;2
+#' Almond Yield Model Function
+#' 
+#' @author Matteo Torres, Amanda Overbye
+#' 
+#' @description The function groups climate data by year and calculates the average February temperature and total January precipitation for each year. It then computes the minimum, maximum, and mean almond yield anomaly of the years in the data set.
+#' 
+#' @param clim_data, A data frame containing climate data with columns for year, month, tmin_c (minimum temperature in Celsius), and precip (precipitation).
+#'  
+#' @returns A vector containing the minimum, maximum, and mean of the almond yield anomaly based on the years of the data set
+#' .
+#' @examples almond_yield_model(clim_data)
 
-
-
-#So we would need to filter the month to equal February
-
-almond_yield_test <- function(clim){
-    group_by(year) %>%
+almond_yield_model <- function(clim_data){
+   almond_year_anomaly <- clim_data %>%
+        group_by(year) %>%
         summarise(
             avg_feb_temp = mean(tmin_c[month == 2],na.rm = TRUE),
             total_jan_precip = sum(precip[month == 1],na.rm = TRUE)
@@ -16,9 +21,9 @@ almond_yield_test <- function(clim){
         mutate(anomaly = -0.015 * avg_feb_temp - 0.0046 * avg_feb_temp^2 -
                    0.07 * total_jan_precip + 0.0043 * total_jan_precip^2 + 0.28)
     
-    anomaly_min <- min(function_test2$anomaly, na.rm = TRUE)
-    anomaly_max <- max(function_test2$anomaly, na.rm = TRUE)
-    anomaly_mean <- mean(function_test2$anomaly, na.rm = TRUE)
+    anomaly_min <- min(almond_year_anomaly$anomaly, na.rm = TRUE)
+    anomaly_max <- max(almond_year_anomaly$anomaly, na.rm = TRUE)
+    anomaly_mean <- mean(almond_year_anomaly$anomaly, na.rm = TRUE)
     
     return(c(anomaly_min, anomaly_max, anomaly_mean))
 }
