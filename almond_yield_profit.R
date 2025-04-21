af1beb3c352d60a119a2710316ca3ca6d7b27f91
Aoverbye <amandaoverbye@gmail.com>
2025-04-21 12:46:44 -0500

    Finished the profit function, need to make graphs.

--- almond_yield_profit.R
+++ almond_yield_profit.R
@@ -10,21 +10,26 @@
 #' .
 #' @examples almond_yield_model(clim_data)
 
-almond_yield_model <- function(clim_data){
-   almond_year_anomaly <- clim_data %>%
+almond_yield_profit <- function(clim_data, almond_profit_ton = 1400){
+    
+    almond_year_anomaly <- clim_data %>%
         group_by(year) %>%
         summarise(
-            avg_feb_temp = mean(tmin_c[month == 2],na.rm = TRUE),
-            total_jan_precip = sum(precip[month == 1],na.rm = TRUE)
+            avg_feb_temp = mean(tmin_c[month == 2], na.rm = TRUE),
+            total_jan_precip = sum(precip[month == 1], na.rm = TRUE)
         ) %>% 
         mutate(anomaly = -0.015 * avg_feb_temp - 0.0046 * avg_feb_temp^2 -
                    0.07 * total_jan_precip + 0.0043 * total_jan_precip^2 + 0.28)
+        
+    anomalies <- c(
+            min  = min(almond_year_anomaly$anomaly, na.rm = TRUE),
+            max  = max(almond_year_anomaly$anomaly, na.rm = TRUE),
+            mean = mean(almond_year_anomaly$anomaly, na.rm = TRUE)
+        )
     
-    anomaly_min <- min(almond_year_anomaly$anomaly, na.rm = TRUE)
-    anomaly_max <- max(almond_year_anomaly$anomaly, na.rm = TRUE)
-    anomaly_mean <- mean(almond_year_anomaly$anomaly, na.rm = TRUE)
     
-    return(c(anomaly_min, anomaly_max, anomaly_mean))
+    profits <- anomalies * almond_profit_ton
     
+    return(profits)
 }
 
